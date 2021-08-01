7ccba50e1601099666d12eed854209f4567d8d3e
Jack Finnis <finnis.j@etoncollege.org.uk>
2021-08-01 14:51:43 +0100

    Initial Commit

---
+++ Shared/iOS_15App.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_15App.swift
+//  Shared
+//
+//  Created by William Finnis on 01/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct iOS_15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
